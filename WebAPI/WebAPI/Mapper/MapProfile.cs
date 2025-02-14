cd8105ae165fc3e8ec387312c118aefe2bfb0d5f
Golov_ng03@student.itstep.org <Golov_ng03@student.itstep.org>
2025-02-14 17:15:18 +0200

    CRUD Operations

---
+++ WebAPI/WebAPI/Mapper/MapProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using WebAPI.Data.Entities;
+using WebAPI.Models.News;
+
+namespace WebAPI.Mapper
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile() 
+        {
+            CreateMap<NewsEntity, NewsItemViewModel>();
+
+            CreateMap<NewsCreateViewModel, NewsEntity>()
+                .ForMember(x => x.ImageUrl, opt => opt.MapFrom(src => src.ImageFile));
+
+            CreateMap<NewsEditViewModel, NewsEntity>()
+                .ForMember(x => x.ImageUrl, opt => opt.Ignore());
+        }
+    }
+}
